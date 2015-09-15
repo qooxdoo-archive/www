eea2bf1de9fc19a8bcf5f2c2ed7744fff9951220
Andreas Ecker <andreas.ecker@1und1.de>
2015-09-15 11:03:10 +0200

    [BUG #9209] Update analytics code

--- news/wp-content/themes/qooxdoo/header.php
+++ news/wp-content/themes/qooxdoo/header.php
@@ -53,15 +53,13 @@
     	wp_head();
     ?>
     <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-415440-1']);
-      _gaq.push(['_setDomainName', 'qooxdoo.org']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-415440-1', 'auto');
+      ga('send', 'pageview');
     </script>
   </head>
   <body <?php body_class("blog"); ?>>
@@ -134,5 +132,5 @@
           <a href="http://qooxdoo.org" title="home">Home</a>
           »
           <a href="http://news.qooxdoo.org">Blog</a>
-        </section>  
+        </section>
         <section id="content">
